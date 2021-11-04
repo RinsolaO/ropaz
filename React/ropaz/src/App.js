db035752fb436d99e9b7720748033751ff64d8e3
rinsolao <olimeed@gmail.com>
2021-11-04 15:59:02 +0100

    Implemented withRouter

--- React/ropaz/src/App.js
+++ React/ropaz/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
+import { Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
--- React/ropaz/src/components/menu-item/MenuItem.jsx
+++ React/ropaz/src/components/menu-item/MenuItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./MenuItem.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`menu-item ${size}`}>
+    <div
+      className={`menu-item ${size}`}
+      onClick={() => history.push(`${match.url}${linkUrl}}`)}
+    >
       <div
         style={{
           backgroundImage: `url(${imageUrl})`,
@@ -18,4 +22,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
