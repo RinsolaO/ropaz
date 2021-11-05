c1c314a69b9a1da097de44a11e395c8a5233ac4d
rinsolao <olimeed@gmail.com>
2021-11-05 14:27:21 +0100

    Added And Styled Header Component, Link To Home And Shop Pages

--- React/ropaz/src/App.js
+++ React/ropaz/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 import { Route, Switch } from "react-router-dom";
 import Shop from "./pages/shop/Shop";
+import Header from "./components/header/Header";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path="/">
             <Homepage />
