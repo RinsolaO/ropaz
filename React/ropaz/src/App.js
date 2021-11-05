a38ac518489f143535d1891129d4fbd98dae051d
rinsolao <olimeed@gmail.com>
2021-11-05 14:07:10 +0100

    Fetched data for Collection Preview Page And Styled

--- React/ropaz/src/App.js
+++ React/ropaz/src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 import { Route, Switch } from "react-router-dom";
+import Shop from "./pages/shop/Shop";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Homepage />
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route exact path="/shop">
+            <Shop />
+          </Route>
+        </Switch>
       </div>
     );
   }
